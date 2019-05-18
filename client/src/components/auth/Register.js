767f92ad7d26f71bf47e86d36a318d33c0b6f3ea
iancugeorge <george.iancu@my.fmi.unibuc.ro>
2019-05-18 14:55:33 +0300

    React Router / Login-Register

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react'
+
+class Register extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    }
+
+    console.log(newUser)
+  }
+
+  render() {
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Inregistrare</h1>
+              <p className="lead text-center">Creaza-ti un cont</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Nume"
+                    name="name"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                  <small className="form-text text-muted"></small>
+                </div>
+                <div className="form-group">
+                  <input type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Parola"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirma Parola"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default Register;
