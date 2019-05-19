1ac22878eb1cbf90cd85a2bc4ab1773081fa52ae
iancugeorge <george.iancu@my.fmi.unibuc.ro>
2019-05-19 20:34:04 +0300

    Redux Store Setup

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
 
@@ -8,8 +10,9 @@ class Register extends Component {
       name: '',
       email: '',
       password: '',
-      password2: ''
-    }
+      password2: '',
+      errors: {}
+    };
 
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
@@ -29,10 +32,14 @@ class Register extends Component {
       password2: this.state.password2
     }
 
-    console.log(newUser)
+    axios.post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="register">
         <div className="container">
@@ -40,43 +47,55 @@ class Register extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Inregistrare</h1>
               <p className="lead text-center">Creaza-ti un cont</p>
-              <form onSubmit={this.onSubmit}>
+              <form noValidate onSubmit={this.onSubmit}>
                 <div className="form-group">
                   <input type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control ,form-control-lg", {
+                      'is-invalid': errors.name
+                    })}
                     placeholder="Nume"
                     name="name"
                     value={this.state.name}
                     onChange={this.onChange}
                   />
+                  {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
                 </div>
                 <div className="form-group">
                   <input type="email"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      'is-invalid': errors.email
+                    })}
                     placeholder="Email"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
+                  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                   <small className="form-text text-muted"></small>
                 </div>
                 <div className="form-group">
                   <input type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      'is-invalid': errors.password
+                    })}
                     placeholder="Parola"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                 </div>
                 <div className="form-group">
                   <input type="password"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      'is-invalid': errors.password2
+                    })}
                     placeholder="Confirma Parola"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onChange}
                   />
+                  {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
